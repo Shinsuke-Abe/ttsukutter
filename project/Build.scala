eeb6cb6f6db36a773401f988999fb962f436bdb0
Shinsuke Abe <mao.instantlife@gmail.com>
2012-03-27 00:59:39 +0900

    Twitter4Jの依存性を追加

--- project/Build.scala
+++ project/Build.scala
@@ -9,10 +9,12 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
+      "org.twitter4j" % "twitter4j-core" % "[2.2,)"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+      // Add your own project settings here
+      resolvers += "twitter4j.org Repository" at "http://twitter4j.org/maven2"
     )
 
 }
