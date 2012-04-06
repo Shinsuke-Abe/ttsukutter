69303e17e6b9d27f0eaf6b1936f6504ffcebf23f
Shinsuke Abe <mao.instantlife@gmail.com>
2012-04-06 14:06:11 +0900

    最新のモデルを反映してクラス定義を作成

--- app/models/TTUser.scala
+++ app/models/TTUser.scala
@@ -8,6 +8,44 @@ package models
  * To change this template use File | Settings | File Templates.
  */
 
-trait TTUser {
+import scala.collection.mutable._
 
+case class TTUser(
+                   id: Option[Long] = None,
+                   name: String,
+                   profile: String,
+                   authInfo: TTUserAuthInfo,
+                   userSiteList: ListBuffer[TTUserSite] = ListBuffer.empty,
+                   favoriteList: ListBuffer[TTUserFavorite] = ListBuffer.empty) {
+  def add {
+
+  }
+
+  def update {
+
+  }
+}
+
+case class TTUserFavorite(ideaId: Int, seqNo: Int)
+
+case class TTUserSite(dispNo: Int, url: String)
+
+case class TTUserAuthInfo(authType: Int,
+                          password: Option[String] = None,
+                          accessToken: Option[String] = None,
+                          tokenSecret: Option[String] = None)
+
+object TTUserFactory {
+  def get(userId: Long) = {
+    //new TTUser
+  }
+
+  def create(
+              name: String,
+              profile: String,
+              authInfo: TTUserAuthInfo,
+              userSiteList: ListBuffer[TTUserSite] = ListBuffer.empty,
+              favoriteList: ListBuffer[TTUserFavorite] = ListBuffer.empty) = {
+    //new TTUser
+  }
 }
---
+++ app/models/TTUserAuthenticateService.scala
@@ -0,0 +1,15 @@
+package models
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mao
+ * Date: 12/04/01
+ * Time: 21:18
+ * To change this template use File | Settings | File Templates.
+ */
+
+object TTUserAuthenticateService {
+  def auth: Boolean = {
+    true
+  }
+}
